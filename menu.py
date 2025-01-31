864234e0b92fe3c4de382e7e58826f4909767af7
Alexis Lerch <alexis.lerch123@gmail.com>
2025-01-31 16:27:37 -0300

    expreciones regulares, generador de contraseña

--- menu.py
+++ menu.py
@@ -1,3 +1,7 @@
+import re
+import secrets
+import string
+
 def menu():
     while True:
         print('______________________________')
@@ -7,6 +11,7 @@ def menu():
         print('3: Expenses tracker')
         print('4: Case converter')
         print('5: Square root')
+        print('6: Generador de contraseña con expreciones regulares')
         print('0: Para salir')
         print('______________________________')
 
@@ -214,6 +219,42 @@ def menu():
             print('______________________________')
             square_root_bisection(int(input('De que numero quieres saber la raiz cuadrada: ')))
             print('______________________________')
+
+        elif opcion == '6':
+            def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
+                # Define the possible characters for the password
+                letters = string.ascii_letters
+                digits = string.digits
+                symbols = string.punctuation
+
+                # Combine all characters
+                all_characters = letters + digits + symbols
+
+                while True:
+                    password = ''
+                    # Generate password
+                    for _ in range(length):
+                        password += secrets.choice(all_characters)
+                    
+                    constraints = [
+                        (nums, r'\d'),
+                        (special_chars, fr'[{symbols}]'),
+                        (uppercase, r'[A-Z]'),
+                        (lowercase, r'[a-z]')
+                    ]
+
+                    # Check constraints        
+                    if all(
+                        constraint <= len(re.findall(pattern, password))
+                        for constraint, pattern in constraints
+                    ):
+                        break
+                
+                return password
+            if __name__ == '__main__':
+                new_password = generate_password()
+                print('Generated password:', new_password)
             
         elif opcion == '0':
             print('Programa finalizado')
