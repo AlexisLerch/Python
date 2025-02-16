d6a18afd3b6f2b1e0d0bccaf4aa01c7fa29feef1
Alexis Lerch <alexis.lerch123@gmail.com>
2025-02-16 19:02:29 -0300

    Agregando el proyecto 8 ordenamiento por fusion

--- menu.py
+++ menu.py
@@ -13,6 +13,7 @@ def menu():
         print('5: Square root')
         print('6: Generador de contraseña con expreciones regulares')
         print('7: Torre de Hanoi con recursividad')
+        print('8: Algoritmo de Orden por Fusión')
         print('0: Para salir')
         print('______________________________')
 
@@ -279,6 +280,49 @@ def menu():
                           
             # initiate call from source A to target C with auxiliary B
             move(NUMBER_OF_DISKS, A, B, C)
+        elif opcion == '8':
+            def merge_sort(array):
+                if len(array) <= 1:
+                    return
+                
+                middle_point = len(array) // 2
+                left_part = array[:middle_point]
+                right_part = array[middle_point:]
+
+                merge_sort(left_part)
+                merge_sort(right_part)
+
+                left_array_index = 0
+                right_array_index = 0
+                sorted_index = 0
+
+                while left_array_index < len(left_part) and right_array_index < len(right_part):
+                    if left_part[left_array_index] < right_part[right_array_index]:
+                        array[sorted_index] = left_part[left_array_index]
+                        left_array_index += 1
+                    else:
+                        array[sorted_index] = right_part[right_array_index]
+                        right_array_index += 1
+                    sorted_index += 1
+
+                while left_array_index < len(left_part):
+                    array[sorted_index] = left_part[left_array_index]
+                    left_array_index += 1
+                    sorted_index += 1
+                
+                while right_array_index < len(right_part):
+                    array[sorted_index] = right_part[right_array_index]
+                    right_array_index += 1
+                    sorted_index += 1
+
+
+            if __name__ == '__main__':
+                numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+                print('Unsorted array: ')
+                print(numbers)
+                merge_sort(numbers)
+                print('Sorted array: ' + str(numbers))
+
         elif opcion == '0':
             print('Programa finalizado')
             break
